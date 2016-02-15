b4a71ac410fd1d120a0bdf8ded2018652a8cd364
Vlad Gryznov <vlad.groznov@gmail.com>
2016-02-16 00:22:02 +0200

    service start ....

--- App/Service.php
+++ App/Service.php
@@ -2,7 +2,24 @@
 
 namespace App;
 
+use App\Exception\ImplementException;
+
 class Service
 {
+
+    public function db()
+    {
+        throw new ImplementException();
+    }
+
+    public function redis()
+    {
+        throw new ImplementException();
+    }
+
+    public function elastic()
+    {
+        throw new ImplementException();
+    }
 
 }
